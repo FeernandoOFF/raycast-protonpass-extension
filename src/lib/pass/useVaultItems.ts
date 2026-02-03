2c8883477b24b211e785ae51549056f8f6ef8ed2
Fernando <fernandooff@Fernandos-MacBook-Pro.local>
2026-02-03 12:51:55 +0000

    Updated :: Hooks to use `UsePromise`

--- src/lib/pass/useVaultItems.ts
+++ src/lib/pass/useVaultItems.ts
@@ -1,40 +1,24 @@
-import { useState, useEffect } from "react";
-import { type Item, useClient } from "./client";
+import { useEffect, } from "react";
+import { useClient } from "./client";
 import { showToast, Toast } from "@raycast/api";
+import { usePromise } from "@raycast/utils";
 
 export function useVaultItems(vaultName: string | null) {
   const client = useClient()
 
-  const [data, setData] = useState<Item[] | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+
+  const  { data, isLoading, error } =  usePromise(async () => {
+    const items = await client.getItems(vaultName);
+    return items
+  });
 
   useEffect(() => {
-    const fetchVaults = async () => {
-      setIsLoading(true);
-      const items = await client.getItems(vaultName);
-      if (items) {
-        try {
-          setData(items);
-        } catch(error: any) {
-          setData(null);
-          showToast({
-            style: Toast.Style.Failure,
-            title: "Error",
-            message: error.message || "Something went wrong",
-          });
-        }
-      }
-      setIsLoading(false);
-    };
-    fetchVaults().catch((error)=> {
-      showToast({
-        style: Toast.Style.Failure,
-        title: "Error",
-        message: error.message || "Something went wrong",
-      });
-    });
-  }, []);
-
+    if(error) showToast(
+      Toast.Style.Failure,
+      "Error",
+      error.message || "Something went wrong"
+    )
+  }, [error])
 
   return { items: data, isLoading };
 }
--- src/lib/pass/useVaults.ts
+++ src/lib/pass/useVaults.ts
@@ -1,39 +1,19 @@
-import { useEffect, useState } from "react";
-import { useClient, Vault } from "./client";
+import { useEffect } from "react";
+import { useClient } from "./client";
 import { showToast, Toast } from "@raycast/api";
+import { usePromise } from "@raycast/utils";
 
 export function useVaults() {
   const client = useClient();
 
-  const [data, setData] = useState<Vault[] | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const { data, isLoading, error } = usePromise(async () => {
+    const vaults = await client.getAllVaults();
+    return vaults;
+  });
 
   useEffect(() => {
-    const fetchVaults = async () => {
-      setIsLoading(true);
-      const vaults = await client.getAllVaults();
-      if (vaults) {
-        try {
-          setData(vaults);
-        } catch(error: any) {
-          setData(null);
-          showToast({
-            style: Toast.Style.Failure,
-            title: "Error",
-            message: error.message || "Something went wrong",
-          });
-        }
-      }
-      setIsLoading(false);
-    };
-    fetchVaults().catch((error)=> {
-      showToast({
-        style: Toast.Style.Failure,
-        title: "Error",
-        message: error.message || "Something went wrong",
-      });
-    });
-  }, []);
+    if (error) showToast(Toast.Style.Failure, "Error", error.message || "Something went wrong");
+  }, [error]);
 
   return { vaults: data, isLoading };
 }
